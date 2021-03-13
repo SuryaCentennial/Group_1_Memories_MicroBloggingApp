725bd44aa7cbe31cf654f1e41121c8948ccad7e3
arorpalak <60004971+arorpalak@users.noreply.github.com>
2021-03-13 01:28:32 -0500

    Profile Details
    get and add user profile details backend

---
+++ dbschema.js
@@ -0,0 +1,47 @@
+let db = {
+  users: [
+    {
+      userId: 'MP782PPH8nbpzRFdODbQA14QPhd2',
+      email: 'arora@gmail.com',
+      handle: 'arora',
+      createdAt: '2021-03-04T04:33:43.701Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is Palak, nice to meet you',
+      website: 'https://user.com',
+      location: 'Toronto, ON'
+    }
+  ],
+  screams: [
+    {
+      userHandle: 'arora',
+      body: 'This is a sample scream',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      likeCount: 5,
+      commentCount: 3
+    }
+  ]
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'I1B8vCOlVaWwDmwz9AZGqbhT60h2',
+    email: 'pal1@gmail.com',
+    handle: 'newone1',
+    createdAt: '2021-03-13T03:20:01.190Z',
+    imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+    bio: 'Hello, my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'Lonodn, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'vRB2Oi1lswMQgUTw6A5d'
+    },
+    {
+      userHandle: 'user',
+      screamId: 'PRf42iYP2spFWtRSXHdO'
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -5,14 +5,18 @@ const app = express();
 const FBAuth = require('./util/fbAuth');
 
 const {getAllScreams, postOneScream} = require('./handlers/screams');
-const {signup,login} = require('./handlers/users');
+const {signup,login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 
 //users route
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 //scream routes
 app.get('/screams',getAllScreams);
 app.post('/scream', FBAuth,postOneScream);
+
 
 exports.api = functions.https.onRequest(app);
