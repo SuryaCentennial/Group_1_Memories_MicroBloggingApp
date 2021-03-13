1a759bb115d560318fb5b297ff6946868fad9046
arorpalak <60004971+arorpalak@users.noreply.github.com>
2021-03-13 00:12:05 -0500

    Image Upload
    Backend logic for image upload

---
+++ dbschema.js
@@ -0,0 +1,23 @@
+let db = {
+  users: [
+    {
+      userId: 'MP782PPH8nbpzRFdODbQA14QPhd2',
+      email: 'arora@gmail.com',
+      handle: 'arora',
+      createdAt: '2021-03-04T04:33:43.701Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is Palak, nice to meet you',
+      website: 'https://user.com',
+      location: 'Toronto, ON'
+    }
+  ],
+  screams: [
+    {
+      userHandle: 'arora',
+      body: 'This is a sample scream',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      likeCount: 5,
+      commentCount: 3
+    }
+  ]
+};
