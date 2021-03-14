d0beec9a73ebd13892da57849c4110ded036d1ea
arorpalak <60004971+arorpalak@users.noreply.github.com>
2021-03-14 04:07:38 -0400

    Comment and Likes
    Backend Logic for comments and liking & unliking blogs.

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,9 +1,9 @@
-const {db} = require('../util/admin');
+const {admin, db} = require('../util/admin');
 const firebaseConfig = require('../util/firebaseConfig');
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
 
-const {validateSignupData, validateLoginData} = require('../util/validators');
+const {validateSignupData, validateLoginData, reduceUserDetails} = require('../util/validators');
 
 exports.signup = (req,res) =>{
     const newUser = {
@@ -16,6 +16,8 @@ exports.signup = (req,res) =>{
     const {valid, errors} = validateSignupData(newUser);
 
     if(!valid) return res.status(400).json(errors);
+
+    const noImg = "no-img.png";
 
     let token, userId;
     db.doc(`/users/${newUser.handle}`).get()
@@ -38,6 +40,7 @@ exports.signup = (req,res) =>{
            handle: newUser.handle,
            email: newUser.email,
            createdAt: new Date().toISOString(),
+           imageUrl: `https://firebasestorage.googleapis.com/v0/b/${firebaseConfig.storageBucket}/o/${noImg}?alt=media`,
            userId
         };
         return db.doc(`/users/${newUser.handle}`).set(userCredentials);
@@ -79,3 +82,124 @@ exports.login =(req, res) =>{
         } else  return res.status(500).json({error: err.code});
     });
 };
+
+// Add user details
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+  
+    db.doc(`/users/${req.user.handle}`)
+      .update(userDetails)
+      .then(() => {
+        return res.json({ message: "Details added successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  };
+
+  // Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.user.handle}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          userData.credentials = doc.data();
+          return db
+            .collection("likes")
+            .where("userHandle", "==", req.user.handle)
+            .get();
+        }
+      })
+      .then((data) => {
+        userData.likes = [];
+        data.forEach((doc) => {
+          userData.likes.push(doc.data());
+        });
+    //     return db
+    //       .collection("notifications")
+    //       .where("recipient", "==", req.user.handle)
+    //       .orderBy("createdAt", "desc")
+    //       .limit(10)
+    //       .get();
+    //   })
+    //   .then((data) => {
+    //     userData.notifications = [];
+    //     data.forEach((doc) => {
+    //       userData.notifications.push({
+    //         recipient: doc.data().recipient,
+    //         sender: doc.data().sender,
+    //         createdAt: doc.data().createdAt,
+    //         screamId: doc.data().screamId,
+    //         type: doc.data().type,
+    //         read: doc.data().read,
+    //         notificationId: doc.id,
+    //       });
+    //     });
+        return res.json(userData);
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  };
+
+// Upload a profile image for user
+exports.uploadImage = (req, res) => {
+    const BusBoy = require("busboy");
+    const path = require("path");
+    const os = require("os");
+    const fs = require("fs");
+  
+    const busboy = new BusBoy({ headers: req.headers });
+  
+    let imageToBeUploaded = {};
+    let imageFileName;
+    // String for image token
+    //let generatedToken = uuid();
+  
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      console.log(fieldname, file, filename, encoding, mimetype);
+      if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+        return res.status(400).json({ error: "Wrong file type submitted" });
+      }
+      // my.image.png => ['my', 'image', 'png']
+      const imageExtension = filename.split(".")[filename.split(".").length - 1];
+      // 32756238461724837.png
+      imageFileName = `${Math.round(
+        Math.random() * 1000000000000
+      ).toString()}.${imageExtension}`;
+      const filepath = path.join(os.tmpdir(), imageFileName);
+      imageToBeUploaded = { filepath, mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+    busboy.on("finish", () => {
+      admin
+        .storage()
+        .bucket()
+        .upload(imageToBeUploaded.filepath, {
+          resumable: false,
+          metadata: {
+            metadata: {
+              contentType: imageToBeUploaded.mimetype,
+              //Generate token to be appended to imageUrl
+             // firebaseStorageDownloadTokens: generatedToken,
+            },
+          },
+        })
+        .then(() => {
+          // Append token to url
+          const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${firebaseConfig.storageBucket}/o/${imageFileName}?alt=media&token=`;
+          return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+        })
+        .then(() => {
+          return res.json({ message: "image uploaded successfully" });
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.status(500).json({ error: "something went wrong" });
+        });
+    });
+    busboy.end(req.rawBody);
+  };
